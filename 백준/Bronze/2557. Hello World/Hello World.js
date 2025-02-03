aa4f6ea8ecd0a2761c164e64a845a0bc5459dcdb
BangDori <44726494+BangDori@users.noreply.github.com>
2025-02-03 22:13:04 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 9408 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,19 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", function (line) {
+    input.push(line);
+  })
+  .on("close", function () {
+    /**
+     * Solution
+     */
+    console.log("Hello World!");
+
+    process.exit();
+  });
