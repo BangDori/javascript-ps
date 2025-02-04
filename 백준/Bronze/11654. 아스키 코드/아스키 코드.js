9e28100ac52fc2e44f472b94bf7e2fe8e5cb0508
BangDori <44726494+BangDori@users.noreply.github.com>
2025-02-04 22:34:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,20 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", function (line) {
+    input.push(line);
+  })
+  .on("close", function () {
+    /**
+     * Solution
+     */
+    const alphabet = input[0];
+    console.log(alphabet.charCodeAt());
+
+    process.exit();
+  });
