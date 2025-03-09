24d092d319c1bbc3f130ff3bfd68c787b33f6513
BangDori <44726494+BangDori@users.noreply.github.com>
2025-03-10 08:59:07 +0900

    [level 2] Title: 가장 큰 수, Time: 162.95 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
-    const answer = numbers.map(numbers => String(numbers)).sort((a,b) => (b+a) - (a+b)).join('');
-    
+    const answer = numbers.map(number => String(number)).sort((a, b) => (b+a)-(a+b)).join("");
+   
     return answer[0] === '0' ? '0' : answer;
 }
