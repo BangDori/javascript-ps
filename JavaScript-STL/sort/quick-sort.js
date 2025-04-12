845afc8068a2b8c0c1520898f4b1df6ea666a971
BangDori <qudwns8865@naver.com>
2025-04-13 02:35:47 +0900

    퀵 소트 - 고정된 피벗

---
+++ JavaScript-STL/sort/quick-sort.js
@@ -0,0 +1,47 @@
+// 퀵소트
+// 피벗을 기준으로 값을 비교하고 위치를 변경한 후에
+// 피벗의 위치를 기준으로 분할하며 정복하는 과정을 거치는 정렬 알고리즘
+
+// 고정 피벗을 사용하는 방식
+
+// 최선: NlogN
+// 평균: NlogN
+// 최악: N^2
+
+// 피벗을 기준으로 정복
+function partition(array, p, right) {
+  const pivot = array[p];
+  let low, high;
+
+  low = p + 1;
+  high = right;
+
+  while (low <= high) {
+    while (array[low] < pivot) low++;
+    while (array[high] > pivot) high--;
+
+    if (low < high) {
+      // 구조 분해 할당을 이용한 swap
+      [array[low], array[high]] = [array[high], array[low]];
+    }
+  }
+
+  [array[p], array[high]] = [array[high], array[p]];
+
+  return high;
+}
+
+function quickSort(array, left, right) {
+  if (left >= right) return;
+
+  // 피벗을 기준으로 값을 비교하고 위치를 변경한 후에
+  const pivot = partition(array, left, right);
+
+  // 피벗을 기준으로 분할
+  quickSort(array, left, pivot - 1);
+  quickSort(array, pivot + 1, right);
+}
+
+const array = [9, 1, 6, 2, 7, 8, 4, 3, 5];
+quickSort(array, 0, array.length - 1);
+console.info(array); // 정렬 결과 출력
