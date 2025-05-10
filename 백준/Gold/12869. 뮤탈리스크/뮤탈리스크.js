dfdf45ad2b9fb80e8507959b1292ce3ef4abc7aa
BangDori <44726494+BangDori@users.noreply.github.com>
2025-05-10 12:46:02 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 200 ms, Memory: 17908 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.js
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.js
@@ -13,6 +13,7 @@ readline
     /**
      * Solution
      */
+
     const n = +input[0];
     const scv = input[1].split(" ").map(Number);
     console.log(solution(n, scv));
@@ -20,53 +21,62 @@ readline
     process.exit();
   });
 
-const damage = [
-  [9, 3, 1],
-  [9, 1, 3],
-  [3, 9, 1],
-  [3, 1, 9],
-  [1, 3, 9],
-  [1, 9, 3],
-];
+const swap = (arr, i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
 
-// 스플래쉬 데미지가 적용되는 모든 경우의 수를 구하자
+function permutation(start, n, r, selected = []) {
+  if (r === selected.length) {
+    return orders.push([...selected]);
+  }
 
-const MAX_HP = 60;
+  for (let i = start; i < n; i++) {
+    swap(damage, start, i);
+    selected.push(damage[start]);
+    permutation(start + 1, n, r, selected);
+    selected.pop();
+    swap(damage, start, i);
+  }
+}
+
+const damage = [9, 3, 1];
+const orders = [];
+
+permutation(0, 3, 3);
+
+const MAX_HP = 61;
 
 function solution(n, scv) {
   if (n === 1) return Math.ceil(scv[0] / 9);
   if (n === 2) scv.push(0);
 
-  const visited = Array.from({ length: MAX_HP + 1 }, () =>
-    Array.from({ length: MAX_HP + 1 }, () => new Array(MAX_HP + 1).fill(false))
-  );
-
-  scv.sort((a, b) => a - b);
-
   const queue = [];
-  queue.push([...scv, 0]);
-  visited[scv[0]][scv[1]][scv[2]] = true;
+  queue.push(scv);
+
+  const dp = Array.from({ length: MAX_HP }, () =>
+    Array.from({ length: MAX_HP }, () => new Array(MAX_HP).fill(-1))
+  );
+  dp[scv[0]][scv[1]][scv[2]] = 0;
 
   while (queue.length) {
+    const nextQueue = [];
     const len = queue.length;
 
     for (let i = 0; i < len; i++) {
-      const [x, y, z, count] = queue.shift();
+      const [x, y, z] = queue.pop();
 
-      if (x + y + z === 0) return count;
+      for (const [dx, dy, dz] of orders) {
+        const nx = Math.max(0, x - dx);
+        const ny = Math.max(0, y - dy);
+        const nz = Math.max(0, z - dz);
 
-      for (const [dx, dy, dz] of damage) {
-        const nx = x - dx >= 0 ? x - dx : 0;
-        const ny = y - dy >= 0 ? y - dy : 0;
-        const nz = z - dz >= 0 ? z - dz : 0;
-
-        if (visited[nx][ny][nz]) continue;
-
-        visited[nx][ny][nz] = true;
-        queue.push([nx, ny, nz, count + 1]);
+        if (dp[nx][ny][nz] === -1) {
+          dp[nx][ny][nz] = dp[x][y][z] + 1;
+          nextQueue.push([nx, ny, nz]);
+        }
       }
     }
+
+    queue.push(...nextQueue);
   }
 
-  return -1;
+  return dp[0][0][0];
 }
