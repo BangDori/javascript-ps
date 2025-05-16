388c77fb0d0c26f811fd7eac3874f4169e83bf58
BangDori <44726494+BangDori@users.noreply.github.com>
2025-05-16 21:40:56 +0900

    [level 2] Title: 구명보트, Time: 36.49 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 13.82 ms
+메모리: 37.6 MB, 시간: 36.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 21:59:33
+2025년 05월 16일 21:40:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,42 +1,29 @@
-// 구명보트 "최대 2명 탑승"
-// 무게 제한 이하만 탑승 가능
-// PS: "최대한 적게" 사용하여 모든 사람을 구출하려고 할 때, 필요한 횟수
+// 50 60 70 79 80
 
-// 역순 정렬 후
-// 작은 사람을 기준으로 큰 사람의 포인터를 계속 오른쪽으로 이동해나가며 비교
-// 이 과정에서 큰 사람과 작은 사람의 합이 limit을 초과한다면, 큰 사람은 결국 혼자 가야하는 사람이니까
-// 빼면서 이동
-    
+// 130
+// 139
+
+// 50 50 70 70 79 80
 
 function solution(people, limit) {
     let answer = 0;
     
-    const rPeople = people.sort((a, b) => b-a);
-    let left = 0, right = rPeople.length-1;
+    people.sort((a, b) => a - b);
     
-    while (left <= right) {
-        const total = rPeople[left] + rPeople[right];
-        
-        if (total > limit) {
-            left++;
-        } else {
-            left++;
-            right--;
-        }
-        
+    let ltr = 0, rtr = people.length - 1;
+    
+    while (ltr <= rtr) {
         answer++;
-    }
         
+        if (ltr === rtr) break;
+        
+        if (people[ltr] + people[rtr] <= limit) {
+            ltr++;
+            rtr--;
+        } else {
+            rtr--;
+        }
+    }
+   
     return answer;
 }
-
-// [40, 40, 40, 40, 40, 1], 41 => 5 
-// [40, 39, 38, 37, 36, 4], 40 => 5
-
-// [40, 40, 40, 40, 40, 4, 3, 2, 1], 40 => 7
-
-// 모든 사람이 혼자 타야하는 경우
-// [40, 40, 40, 40], 79 => 4
-
-// 모든 사람이 2명씩 타서 가는 경우
-// [40, 40, 40, 40], 80 => 2
