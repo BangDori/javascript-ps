84ff91c1cf4c1c6508429957af343f0ac2dac8d7
BangDori <44726494+BangDori@users.noreply.github.com>
2025-05-24 10:08:11 +0900

    [level 3] Title: 입국심사, Time: 723.60 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,23 +1,33 @@
-const MAX_TIME = BigInt(1e18) + 1n;
+// 10:02 ~ 
+
+// 모든 사람이 심사를 받는데 걸리는 시간을 "최소"로
+
+// 1 <= 입국심사를 기다리는 사람 <= 1,000,000,000
+// 1 <= 한 명을 심사하는데 걸리는 시간 <= 1,000,000,000
+// 1 <= 심사관 <= 100,000
+
+// 비어있는 심사대로 이동?
+
+// 현재 시간이 n일 때, 몇명의 입국 심사를 하였는지를 확인
+// 시간 binary search로
 
 function solution(n, times) {
-    let left = 1n, right = MAX_TIME;
+    let left = 1n, right = BigInt(1e18 + 1);
     
     while (left < right) {
-        const mid = (left + right) / 2n;
-        
-        let total_count = 0n;
+        const currTime = (left + right) / 2n;
+        let completed = 0n;
         
         for (const time of times) {
-            total_count += (mid / BigInt(time));
+            completed += currTime / BigInt(time);
         }
         
-        if (total_count >= BigInt(n)) {
-            right = mid;
+        if (completed >= n) {
+            right = currTime;
         } else {
-            left = mid + 1n;
+            left = currTime + 1n;
         }
     }
-
-    return Number(right);
+    
+    return right;
 }
